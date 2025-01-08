44f817e52b4151e7e8c650ef2162a13ab697bf30
Anurag Swarnakar <anuragswarnakar626@gmail.com>
2025-01-08 12:07:30 +0530

    "Added Title"

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles/globals.css";
-
+import Head from "next/head";
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Task Manager</title>
+        <meta
+          name="description"
+          content="A simple task manager built with Next.js and React."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />)
+    </>
+  );
 }
 
 export default App;
